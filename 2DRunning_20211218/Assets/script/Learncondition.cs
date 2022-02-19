411b159dc8092dc6bc0bf62a80dc3f4fe51de552
cyrus0119 <96644235+cyrus0119@users.noreply.github.com>
2022-02-19 16:12:19 +0800

    updata

--- 2DRunning_20211218/Assets/script/Learncondition.cs
+++ 2DRunning_20211218/Assets/script/Learncondition.cs
@@ -13,7 +13,7 @@ public class Learncondition : MonoBehaviour
     private void Start()
     {
         // if 語法
-        // in (布林值) {程式區塊、演算法、陳述式}
+        // if (布林值) {程式區塊、演算法、陳述式}
         //快速完成 if + tab*2
         //布林值等於 true 會執行{}內的程式
         //          false 不執行
@@ -37,14 +37,19 @@ public class Learncondition : MonoBehaviour
 
     private void Update()
     {
+        //else if 必須在 if 下方 沒有數量限制
+        //else if (布林值) {程式區塊、演算法、陳述式}
+        //連擊等於10攻擊力加10
         if (combo == 10) 
         {
             print("攻擊力加10");
         }
+        //連擊等於100攻擊力加100
         else if (combo == 100)
         {
             print("攻擊力加100");
         }
+        //連擊等於500攻擊力加500
         else if (combo == 500)
         {
             print("攻擊力加500");
