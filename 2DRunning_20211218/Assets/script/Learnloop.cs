43609b952b1172306095cfe4553b5226fd4648cc
cyrus0119 <96644235+cyrus0119@users.noreply.github.com>
2022-02-19 19:37:24 +0800

    update

--- 2DRunning_20211218/Assets/script/Learnloop.cs
+++ 2DRunning_20211218/Assets/script/Learnloop.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 /// <summary>
 /// 迴圈
@@ -12,12 +13,14 @@ public class Learnloop : MonoBehaviour
     //處理重複程式
     private void Start()
     {
-        //輸出5次哈囉
+        //輸出5次哈囉 非迴圈
+        /*
         print("哈囉");
         print("哈囉");
         print("哈囉");
         print("哈囉");
         print("哈囉");
+        */
 
         // while 迴圈語法與 if 相同
         // if (布林值) {程式區塊};
@@ -34,6 +37,17 @@ public class Learnloop : MonoBehaviour
         {
             print("哈囉");
             count++;
+        }
+
+        //for迴圈(初始值; 布林值; 迴圈結束會執行程式)
+        for (int i = 0; i < 5; i++)
+        {
+            print("哈囉" + i );
+        }
+        //輸出數字0-100 以5遞增
+        for (int i = 0; i < 101; i+=5)
+        {
+            print("輸出數字:" + i);
         }
     }
 
